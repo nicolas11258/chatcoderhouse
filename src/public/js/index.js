212c135dbfa9764ab425efb3ce967a8fa098f322
Nicolas <nicolas@gmail.com>
2023-11-06 18:49:37 -0500

    first commit

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -5,9 +5,7 @@ socket.on('messageLogs', data => {
     let log = document.getElementById('messageLogs');
     let messages = '';
     data.forEach(message => {
-        if (message.user && message.message){
-            messages = `${messages} ${message.user} dice: ${message.message} </br>`;
-        }
+        messages = `${messages} ${message.user} dice: ${message.message} </br>`;
     });
     log.innerHTML = messages;
 });
